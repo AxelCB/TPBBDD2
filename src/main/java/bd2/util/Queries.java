60e7928509a92f30fdfc801b87822ddc51d09c10
Ruta, Santiago <ruta2366@gmail.com>
2015-06-03 00:15:19 -0300

    final entrega 3

--- src/main/java/bd2/util/Queries.java
+++ src/main/java/bd2/util/Queries.java
@@ -11,6 +11,7 @@ import org.hibernate.cfg.Configuration;
 
 import bd2.model.Pizarra;
 import bd2.model.Tarea;
+import bd2.model.Usuario;
 
 public class Queries {
 	
@@ -25,7 +26,7 @@ public class Queries {
 			session = factory.openSession();
 
 			listarPizzarras(session);
-			listarTareasConDescripcion(session,"%site member%");
+			listarTareasConDescripcion(session,"%news%");
 			pizarraConMasTareas(session);
 			emailsDeAdministradorDeProyectosConMasDeUnaPizarraArchivada(session);
 			tareasQueHayanPasadoPorPizarra(session,"backlogproyecto8149");  
@@ -47,57 +48,70 @@ public class Queries {
 		System.out.println("--------------------------------------");
 		System.out.println("a) Listar los nombres de todas las pizarras.Imprimir en consola: ”Pizarra: <nombre>”");
 		System.out.println("--------------------------------------");
-		Query query = session.createQuery("SELECT nombre FROM Pizarra");
+		Query query = session.createQuery(""
+				+ "FROM "
+					+ "Pizarra");
 		String impresion = "Pizarra: ";		
 		List<Object> resultados = Queries.ejecutar(session, query);
 		for (Object object : resultados) {
-			System.out.println(impresion+(String)object);
+			System.out.println(impresion+((Pizarra)object).getNombre());
 		}
-		System.out.println("--------------------------------------");
 	}
 	
 	public static void listarTareasConDescripcion(Session session,String descripcionParametro){
 		System.out.println("--------------------------------------");
 		System.out.println("b) Listar las tareas cuya descripción contenga una secuencia específica de caracteres (enviada como parámetro).Imprimir en consola: ”Tarea: <descripción>”");
 		System.out.println("--------------------------------------");
-		Query query = session.createQuery("FROM Tarea as t WHERE descripcion LIKE :descripcionParametro").
+		Query query = session.createQuery(""
+				+ "FROM "
+					+ "Tarea "
+				+ "WHERE "
+					+ "descripcion LIKE :descripcionParametro").
 				setString("descripcionParametro", descripcionParametro);
 		String impresion = "Tarea: ";		
 		List<Object> resultados = Queries.ejecutar(session, query);
 		for (Object object : resultados) {
 			System.out.println(impresion+((Tarea)object).getDescripcion());
 		}
-		System.out.println("--------------------------------------");
+		System.out.println("");
 	}
 	
 	public static void pizarraConMasTareas(Session session){
 		System.out.println("--------------------------------------");
 		System.out.println("c)Obtener la Pizarra que tenga más tareas. Imprimir ”Pizarra con más tareas: <nombre> (<cantidad de tareas> tareas)”");
 		System.out.println("--------------------------------------");
-		Query query = session.createQuery("FROM Pizarra as p ORDER BY p.tareas.size DESC ").setMaxResults(1);
+		Query query = session.createQuery(""
+				+ "FROM "
+					+ "Pizarra as p "
+				+ "ORDER BY "
+					+ "p.tareas.size DESC ").setMaxResults(1);
 		List<Object> resultados = Queries.ejecutar(session, query);
 		for (Object object : resultados) {
 			System.out.println("Pizarra con más tareas: "+((Pizarra)object).getNombre()+" ("+((Pizarra)object).getTareas().size()+" tareas)");
 		}
-		System.out.println("--------------------------------------");
-
+		System.out.println("");
 	}
 	
 	public static void emailsDeAdministradorDeProyectosConMasDeUnaPizarraArchivada(Session session){
 		System.out.println("--------------------------------------");
 		System.out.println("d)Obtener los emails de los administradores de los proyectos que tengan al menos una pizarra archivada. Imprimir “Administrador: <email>”");
 		System.out.println("--------------------------------------");
-		Query query = session.createQuery("SELECT DISTINCT(u.email) "
-			+ "FROM Proyecto as p "
-				+ "INNER JOIN p.perfiles as pda "
-				+ "INNER JOIN pda.usuario as u "
-			+ "WHERE p.pizarrasArchivadas.size > 0");
+		Query query = session.createQuery(""
+				+ "SELECT "
+					+ "u "
+				+ "FROM "
+					+ "Proyecto as p "
+					+ "JOIN p.pizarrasArchivadas as pa "
+					+ "JOIN p.perfiles as pe "
+					+ "JOIN pe.usuario as u "
+				+ "WHERE "
+					+ "pe.class = PerfilDeAdministrador ");
 		String impresion = "Administrador: ";		
 		List<Object> resultados = Queries.ejecutar(session, query);
 		for (Object object : resultados) {
-			System.out.println(impresion+(String)object);
+			System.out.println(impresion+((Usuario)object).getNombre());
 		}
-		System.out.println("--------------------------------------");
+		System.out.println("");
 	}
 	
 	public static void tareasQueHayanPasadoPorPizarra(Session session,String descripcionParametro){
@@ -105,44 +119,62 @@ public class Queries {
 		System.out.println("e)Obtener las tareas que hayan pasado por la pizarra cuyo nombre contenga una secuencia de caracteres enviada como parámetro. Imprimir “Tarea: <descripción>”");
 		System.out.println("--------------------------------------");
 		Query query = session.
-				createQuery("SELECT t FROM Tarea as t "
-							+ "INNER JOIN t.pasos as pa "
-							+ "INNER JOIN pa.pizarra as pi "
-						+ "WHERE pi.nombre LIKE :descripcionParametro").
-						setString("descripcionParametro", descripcionParametro);
+				createQuery(""
+						+ "SELECT "
+							+ "t "
+						+ "FROM "
+							+ "Tarea as t "
+							+ "JOIN t.pasos as pa "
+							+ "JOIN pa.pizarra as pi "
+						+ "WHERE "
+							+ "pi.nombre LIKE :descripcionParametro");
+		query.setString("descripcionParametro", descripcionParametro);
 		String impresion = "Tarea: ";		
 		List<Object> resultados = Queries.ejecutar(session, query);
 		for (Object object : resultados) {
 			System.out.println(impresion+((Tarea)object).getDescripcion());
 		}
-		System.out.println("--------------------------------------");
+		System.out.println("");
 	}
 	
 	public static void tareasConMasPasosQue(Session session,Integer cantPasos){
 		System.out.println("--------------------------------------");
 		System.out.println("f)Obtener las tareas que hayan sido cambiadas de pizarra más de un número veces enviado como parámetro. Imprimir “Tarea: <descripción> (<cantidad de pasos> pasos)”");
 		System.out.println("--------------------------------------");
-		Query query = session.createQuery("FROM Tarea as t WHERE t.pasos.size > :cantPasos ").setInteger("cantPasos", cantPasos);
+		Query query = session.createQuery(""
+				+ "FROM "
+					+ "Tarea as t "
+				+ "WHERE "
+					+ "t.pasos.size > :cantPasos ");
+		query.setInteger("cantPasos", cantPasos);
 		String impresion = "Tarea: ";		
 		List<Object> resultados = Queries.ejecutar(session, query);
 		for (Object object : resultados) {
 			System.out.println(impresion+((Tarea)object).getDescripcion()+"("+((Tarea)object).getPasos().size()+" pasos)");
 		}
-		System.out.println("--------------------------------------");
+		System.out.println("");
 	}
 	
 	public static void pizarrasConTareasDeAmbosTipos(Session session){
 		System.out.println("--------------------------------------");
 		System.out.println("g)Obtener las pizarras que tengan tareas tanto de investigación como de desarrollo. Imprimir ”Pizarra: <nombre>” ");
 		System.out.println("--------------------------------------");
-		Query query = session.createQuery("SELECT distinct(p) FROM Pizarra as p,TareaDeDesarrollo tdd,TareaDeInvestigacion tdi "
-				+ "WHERE (tdd in elements(p.tareas) AND tdi in elements(p.tareas))");
+		Query query = session.createQuery(""
+				+ "SELECT "
+					+ "distinct(p) "
+				+ "FROM "
+					+ "Pizarra as p, " 
+					+ "TareaDeDesarrollo tdd, "
+					+ "TareaDeInvestigacion tdi "
+				+ "WHERE "
+					+ "(tdd in elements(p.tareas) "
+					+ "AND tdi in elements(p.tareas))");
 		String impresion = "Pizarra: ";		
 		List<Object> resultados = Queries.ejecutar(session, query);
 		for (Object object : resultados) {
 			System.out.println(impresion+((Pizarra)object).getNombre());
 		}
-		System.out.println("--------------------------------------");
+		System.out.println("");
 	}
 	
 	public static void tareasPizarrasVencidasMarzo(Session session){
@@ -155,28 +187,23 @@ public class Queries {
 		System.out.println("--------------------------------------");
 		System.out.println("h)Obtener las pizarras que tengan tareas vencidas en marzo, es decir que sus fechas límite estén dentro marzo de 2015 y no estén completas. Imprimir “Pizarra: <nombre>”");
 		System.out.println("--------------------------------------");
-		Query query = session.createQuery("SELECT DISTINCT(p) FROM Pizarra as p INNER JOIN p.tareas as t "
-				+ "WHERE t.fechaLimite BETWEEN :startDate AND :endDate )")
-				.setDate("startDate", startDate.getTime())
-				.setDate("endDate", endDate.getTime());
+		Query query = session.createQuery(""
+				+ "SELECT "
+					+ "DISTINCT(p) "
+				+ "FROM "
+					+ "Pizarra as p "
+					+ "JOIN p.tareas as t "
+				+ "WHERE "
+					+ "t.fechaLimite BETWEEN :startDate AND :endDate )");
+		query.setDate("startDate", startDate.getTime());
+		query.setDate("endDate", endDate.getTime());
 		String impresion = "Pizarra: ";		
 		List<Object> resultados = Queries.ejecutar(session, query);
 		for (Object object : resultados) {
 			System.out.println(impresion+((Pizarra)object).getNombre());
 		}
-		System.out.println("--------------------------------------");
+		System.out.println("");
 	}
-	
-//	public static void listarGenerico(Session session, Query query, String enunciado, String impresion){
-//		System.out.println("--------------------------------------");
-//		System.out.println(enunciado);
-//		System.out.println("--------------------------------------");
-//		List<Object> resultados = Queries.ejecutar(session, query);
-//		for (Object object : resultados) {
-//			System.out.println(impresion+(String)object);
-//		}
-//		System.out.println("--------------------------------------");
-//	}
 	
 	@SuppressWarnings("unchecked")
 	public static List<Object> ejecutar(Session session,Query query){	
